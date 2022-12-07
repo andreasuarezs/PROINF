8fabd15e19a74ee196987eccc1ac0b96a10852c2
Andrea Suárez <91388149+andreasuarezs@users.noreply.github.com>
2022-12-07 09:53:03 +0100

    Add files via upload

---
+++ src/CompaniaPublica.java
@@ -0,0 +1,26 @@
+/**
+ * Clase Companyia
+ * @author Josep Molet y Andrea Suarez
+ *
+ */
+public class CompaniaPublica extends Propiedad {
+
+	/**
+	 * Constructor por defecto
+	 */
+	public CompaniaPublica() 
+	{
+		this("", 0);
+	}
+	
+	/**
+	 * Constructor parametrizado
+	 * @param nombre Nombre de la compania
+	 * @param precio Precio de la compania
+	 */
+	public CompaniaPublica(String nombre, int precio) 
+	{
+		super(nombre, precio);
+	}
+	
+}
---
+++ src/Unica.java
@@ -0,0 +1,17 @@
+/**
+ * Clase Unica
+ * @author Josep Molet y Andrea Suarez
+ *
+ */
+public class Unica extends Especial {
+
+	/**
+	 * Contructor parametrizado
+	 * @param descripcion Nombre de la casilla
+	 */
+	public Unica(String descripcion)
+	{
+		super(descripcion);
+	}
+	
+}
